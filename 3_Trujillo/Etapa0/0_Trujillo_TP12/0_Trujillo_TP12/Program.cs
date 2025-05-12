6b001b53ad1aa2b1de4f8bb3d050bba5ed5c8105
Sollijurt <santytrujillorosso@gmail.com>
2025-05-12 10:15:22 -0300

    Add files via upload

---
+++ 3_Trujillo/Etapa0/0_Trujillo_TP12/0_Trujillo_TP12/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_Trujillo_TP12
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Ingrese 3 letras");
+            string let = Console.ReadLine();
+            Console.WriteLine(let[2]+ "" + let[1] + "" + let[0]);
+            Console.ReadKey();
+
+        }
+    }
+}
