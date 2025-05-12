87c600cca8509fce21cf550cba28f1784d0ad1dc
Sollijurt <santytrujillorosso@gmail.com>
2025-05-12 09:14:46 -0300

    Add files via upload

---
+++ 3_Trujillo/0_Trujillo_TP8/0_Trujillo_TP8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_Trujillo_TP8
+{
+    class Program
+    {
+        static void Main()
+        {
+            double num1, num2;
+            Console.WriteLine("Ingrese 2 números");
+            num1 = int.Parse(Console.ReadLine());
+            num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("El resto de los números es " + (num1 % num2));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 3_Trujillo/0_Trujillo_TP9/0_Trujillo_TP9/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_Trujillo_TP9
+{
+    class Program
+    {
+        static void Main()
+        {
+            double radio;
+            Console.WriteLine("Ingrese el radio de la esfera");
+            radio = int.Parse(Console.ReadLine());
+            Console.WriteLine("La superficie de la esfera es " + (4 * Math.PI * Math.Pow(radio, 2)));
+            Console.WriteLine("El volumen de la esfera es " + (Math.PI * Math.Pow(radio, 3) * 4 / 3));
+            Console.ReadKey();
+        }
+    }
+}
