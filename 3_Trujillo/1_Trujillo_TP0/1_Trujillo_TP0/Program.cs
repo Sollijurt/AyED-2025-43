0e6ed900675a8dd115fc43457b3bd14ae29b9379
Sollijurt <santytrujillorosso@gmail.com>
2025-05-12 09:23:42 -0300

    Add files via upload

---
+++ 3_Trujillo/1_Trujillo_TP0/1_Trujillo_TP0/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Trujillo_TP0
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Ingrese su edad");
+            int edad = int.Parse(Console.ReadLine());
+            if (edad > 18)
+            {
+                Console.WriteLine("Usted es mayor de edad");
+            }
+            else
+            {
+                Console.WriteLine("Usted es menor de edad");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
