1ef8a95300be67bfa09c5fdac190fafe724b46e6
Sollijurt <santytrujillorosso@gmail.com>
2025-05-12 10:17:41 -0300

    Add files via upload

---
+++ 3_Trujillo/Etapa1/1_Trujillo_TP1/1_Trujillo_TP1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Trujillo_TP1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Ingrese una contraseña");
+            string contra = Console.ReadLine().ToUpper();
+            Console.WriteLine("Escriba su contraseña");
+            string contra2 = Console.ReadLine().ToUpper();
+            if (contra == contra2){
+                Console.WriteLine("Contraseña es verdadera");
+            }
+            else
+            {
+                Console.WriteLine("Contraseña es falsa");
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 3_Trujillo/Etapa1/1_Trujillo_TP3/1_Trujillo_TP3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Trujillo_TP3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Ingrese un número entero");
+            int num = int.Parse(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("El numero es par");
+            }
+            else
+            {
+                Console.WriteLine("El número es impar");
+            }
+            Console.ReadKey();
+        }
+    }
+}
