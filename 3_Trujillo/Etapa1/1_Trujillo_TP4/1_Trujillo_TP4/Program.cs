81e2c73cea006d354e9e7af18b5ff0d894814156
Sollijurt <santytrujillorosso@gmail.com>
2025-05-12 10:19:15 -0300

    Add files via upload

---
+++ 3_Trujillo/Etapa1/1_Trujillo_TP4/1_Trujillo_TP4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Trujillo_TP4
+{
+    class Program
+    {
+        static void Main()
+        {
+            Boolean beca = false;
+            Console.WriteLine("Ingrese su edad");
+            int edad = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese su ingresos mensuales");
+            Int64 ingresos = Int64.Parse(Console.ReadLine());
+
+            if (edad == 19 && ingresos >= 50000)
+            {
+                beca = true;
+            }
+            else if (edad < 19 && ingresos == 0)
+            {
+                beca = true;
+            }
+            else if (edad > 19 && ingresos <= 100000)
+            {
+                beca = true;
+
+            }
+            
+            if (beca)
+            {
+                Console.WriteLine("Usted puede cobrar la beca");
+            }
+            else
+            {
+                Console.WriteLine("Usted NO puede cobrar la beca");
+            }
+            Console.ReadKey();
+        }
+    }
+}
