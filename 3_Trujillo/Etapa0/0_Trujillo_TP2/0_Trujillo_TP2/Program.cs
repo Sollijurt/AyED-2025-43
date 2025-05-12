091db1b256b22f7344ceec5f28587cf197391fd3
Sollijurt <santytrujillorosso@gmail.com>
2025-05-12 09:32:47 -0300

    Add files via upload

---
+++ 3_Trujillo/Etapa0/0_Trujillo_TP2/0_Trujillo_TP2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_Trujillo_TP2
+{
+    class Program
+    {
+        static void Main()
+        {
+            string nombre = "Santiago";
+            int edad = 16;
+            Console.WriteLine("Hola " + nombre + " Tu edad es " + edad);
+            Console.ReadKey();
+        }
+    }
+}
