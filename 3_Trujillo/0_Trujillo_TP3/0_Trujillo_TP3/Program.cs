eb7a5364dd27631dfc5258c412cdca7c86451e79
Sollijurt <santytrujillorosso@gmail.com>
2025-05-12 09:12:33 -0300

    Add files via upload

---
+++ 3_Trujillo/0_Trujillo_TP3/0_Trujillo_TP3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_Trujillo_TP3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Ingrese su nombre");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese su edad");
+            string edada = Console.ReadLine();
+            int edad = int.Parse(edada);
+            Console.WriteLine("Hola " + nombre + " Tu edad es " + edad);
+            Console.ReadKey();
+        }
+    }
+}
