539c855f1e32d80325d1456288e9ac424c2291b8
Sollijurt <santytrujillorosso@gmail.com>
2025-06-08 21:20:52 -0300

    Add files via upload

---
+++ 3_Trujillo/Etapa2/2_Trujillo_TP8/2_Trujillo_TP8/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Ingrese las filas de la matriz");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese las columnas de la matriz");
+int m = int.Parse(Console.ReadLine());
+
+int[,] matriz = new int[n, m];
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        Console.Write(matriz[i, j] + " ");
+    }
+    Console.WriteLine("");
+}
---
+++ 3_Trujillo/Etapa2/2_Trujillo_TP9/2_Trujillo_TP9/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Ingrese la cantidad de filas");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese la cantidad de columnas");
+int m = int.Parse(Console.ReadLine());
+
+Random ale = new Random();
+
+int[,] matriz = new int[n,m];
+
+for (int i = 0; i < n; i++)
+{
+     for(int j = 0; j < m; j++)
+    {
+        matriz[i, j] = ale.Next(1, 100);
+    }
+}
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        Console.Write(matriz[i, j] + " ");
+    }
+    Console.WriteLine("");
+}
