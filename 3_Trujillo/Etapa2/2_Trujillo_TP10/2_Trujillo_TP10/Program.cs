7085c8e206d0b3d91b9711216af92d8d4d95e1e9
Sollijurt <santytrujillorosso@gmail.com>
2025-06-08 21:18:49 -0300

    Add files via upload

---
+++ 3_Trujillo/Etapa2/2_Trujillo_TP10/2_Trujillo_TP10/Program.cs
@@ -0,0 +1,28 @@
+﻿//Sumar dos matrices de igual tamaño nxn, almacenarlas en una nueva e imprimirla por pantalla.
+
+Console.WriteLine("Ingrese el tamaño de la matriz");
+int n = int.Parse(Console.ReadLine());
+Random ale = new Random();
+
+int[,] matriz1 = new int[n, n];
+int[,] matriz2 = new int[n, n];
+int[,] resu = new int[n, n];
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        matriz1[i, j] = ale.Next(1, 100);
+        matriz2[i, j] = ale.Next(1, 100);
+        resu[i, j] = matriz1[i, j] + matriz2[i, j];
+    }
+}
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write(resu[i, j] + " ");
+    }
+    Console.WriteLine(" ");
+}
---
+++ 3_Trujillo/Etapa2/2_Trujillo_TP11/2_Trujillo_TP11/Program.cs
@@ -0,0 +1,47 @@
+﻿
+
+Console.WriteLine("n matriz");
+int n = int.Parse(Console.ReadLine());
+int[,] matriz = new int[n,n];
+int[,] resu = new int[n, n];
+Random ale = new Random();
+
+
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        matriz[i, j] = ale.Next(0, 100);
+    }
+}
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write(matriz[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        resu[j, n - 1 - i] = matriz[i, j];
+    }
+}
+
+
+Console.WriteLine("");
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        
+        Console.Write(resu[i, j] + " ");
+
+    }
+    Console.WriteLine();
+}
