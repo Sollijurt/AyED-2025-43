63ff009eceade77d88c62aed0e808cf933983f45
Sollijurt <santytrujillorosso@gmail.com>
2025-09-24 11:52:17 -0300

    Add files via upload

---
+++ 3_Trujillo/Etapa3/3_Trujillo_TP5/3_Trujillo_TP5/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_Trujillo_TP5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hpHornet = 10;
+            int hpMaxHornet = 10;
+            int atkHornet = 3;
+            int hpEnemigo = 12;
+
+            Console.WriteLine("=== Hollow Knight: Simulador básico ===");
+
+            while (hpHornet > 0 && hpEnemigo > 0)
+            {
+                MostrarEstado(hpHornet, hpEnemigo);
+
+                Console.Write("\nAcción (atacar/curar/salir):");
+                string accion = Console.ReadLine();
+
+                if (accion == "atacar")
+                {
+                    hpEnemigo = atacar(atkHornet, hpEnemigo);
+                }
+                else if (accion == "curar")
+                {
+                    hpHornet = curar(hpHornet, hpMaxHornet);
+                }
+                else if (accion == "salir")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Acción inválida");
+                }
+                if (hpEnemigo > 0)
+                {
+                    hpHornet -= 2;
+                    Console.WriteLine("El enemigo contraataca y hace 2 de daño!");
+                }
+
+            }
+            Console.WriteLine("\n === Resultado ===");
+            if (hpHornet <= 0) Console.WriteLine("Hornet ha caido...");
+            else if (hpEnemigo <= 0) Console.WriteLine("Hornet ha ganado!");
+            else Console.WriteLine("Batalla interrumpida");
+            Console.ReadKey();
+        }
+
+        static int atacar(int ataque, int vidaE)
+        {
+            int vidafinal = vidaE - ataque;
+            return vidafinal;
+        }
+        static int curar(int vidaH, int MaxiVidaH)
+        {
+            int vidafinal;
+            vidafinal = vidaH + 5;
+            if ((vidaH + 5) >= MaxiVidaH)
+            {
+                vidafinal = vidaH + (MaxiVidaH - vidaH);
+            }
+
+            return vidafinal;
+        }
+        static void MostrarEstado(int Vidah, int VidaE)
+        {
+            Console.WriteLine("Hornet HP: " + Vidah + " | " + "Enemigo HP: " + VidaE);
+
+        }
+    }
+}
---
+++ 3_Trujillo/Etapa3/3_Trujillo_TP6/3_Trujillo_TP6/Program.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_Trujillo_TP6
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("=== Mini Balatro (versión simplificado) ===");
+
+            string[] mano = GenerarManoAleatoria();
+
+            string tipo = TipoMano(mano);
+
+            int basePts = puntajebase(mano);
+
+            double mult = multiplicador(tipo);
+
+            double total = basePts * mult;
+
+            bool Joker2X = true;
+
+            bool jokermas10 = true;
+
+            total = AplicarJokers(total, Joker2X, jokermas10);
+
+
+            MostrarResumen(mano, tipo, basePts, mult, total);
+            Console.ReadKey();
+        }
+        static string[] GenerarManoAleatoria()
+        {
+            Random rand = new Random();
+
+            string[] ValorCartas = { "A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2" };
+            string[] PalosAsignados = { "H", "D", "C", "S" };
+            string[] mano = new string[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                mano[i] = ValorCartas[rand.Next(0, 12)] + PalosAsignados[rand.Next(0, 3)];
+            }
+
+            return mano;
+        }
+        static string TipoMano(string[] m)
+        {
+            string[] mano = { "Poker", "Full", "Trio", "Par", "Nada" };
+            string ayuda2 = "";
+            int num = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                string ayuda = m[i];
+                ayuda2 += ayuda[0];
+            }
+            switch (busquedamano(ayuda2))
+            {
+                case 6:
+                    num = 0;
+                    break;
+                case 4:
+                    num = 1;
+                    break;
+                case 3:
+                    num = 2;
+                    break;
+                case 1:
+                    num = 3;
+                    break;
+                default:
+                    num = 4;
+                    break;
+            }
+
+            return mano[num];
+        }
+        static int busquedamano(string m)
+        {
+            int conta = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = i + 1; j < 5; j++)
+                {
+                    if (m[i] == m[j])
+                    {
+                        conta++;
+                    }
+                }
+            }
+            return conta;
+        }
+        static int puntajebase(string[] m)
+        {
+            char ayudin;
+            string ayuda2 = "";
+            int suma = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                string ayuda = m[i];
+                ayuda2 += ayuda[0];
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                ayudin = ayuda2[i];
+                switch (ayudin)
+                {
+                    case 'A':
+                        suma = suma + 14;
+                        break;
+                    case 'K':
+                        suma = suma + 13;
+                        break;
+                    case 'Q':
+                        suma = suma + 12;
+                        break;
+                    case 'J':
+                        suma = suma + 11;
+                        break;
+                    case 'T':
+                        suma = suma + 10;
+                        break;
+                    default:
+                        suma = suma + (ayudin - '0');
+                        break;
+                }
+            }
+
+            return suma;
+        }
+        static double multiplicador(string tipo)
+        {
+            double num = 0;
+            switch (tipo)
+            {
+                case "Poker":
+                    num = 4.0;
+                    break;
+                case "Full":
+                    num = 3.5;
+                    break;
+                case "Trio":
+                    num = 2.5;
+                    break;
+                case "Par":
+                    num = 1.5;
+                    break;
+                default:
+                    num = 1.0;
+                    break;
+            }
+
+            return num;
+        }
+        static double AplicarJokers(double total, bool jokerx2, bool joker10)
+        {
+            if (jokerx2)
+            {
+                total = total * 2;
+            }
+            if (joker10)
+            {
+                total = total + 10;
+            }
+
+            return total;
+        }
+        static void MostrarResumen(string[] m, string tipo, int puntos, double mult, double total)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write(m[i] + " ");
+            }
+            Console.WriteLine("\n" + "El tipo de mano es: " + tipo);
+            Console.WriteLine("Las fichas bases son: " + puntos);
+            Console.WriteLine("El multi del tipo de mano: " + mult);
+            Console.WriteLine("El total es: " + total);
+        }
+
+    }
+}
+
